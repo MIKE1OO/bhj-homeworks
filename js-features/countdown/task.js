b086ddb5519b2ef4eff8740bc395b2b9600517e8
MIKE100 <mich9745yt@gmail.com>
2024-01-22 20:38:37 +0300

    first commit variables

---
+++ js-features/countdown/task.js
@@ -0,0 +1,11 @@
+const countDown = function() {
+    const timer = document.getElementById("timer");
+    if (timer.textContent == 0) {
+        alert("Вы победили в конкурсе");
+        clearInterval(interval);
+    } else {
+        timer.textContent--;
+    }
+}
+
+let interval = setInterval(countDown, 1000);
